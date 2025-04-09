732cf6804cd9f41ed45a20bddb32e2b5b3fb5715
M. Early Maulana <142871177+earlysme2703@users.noreply.github.com>
2025-04-10 01:07:44 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Web Pengelolaan Nilai') }} - Login</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         .clip-trapezoid {
